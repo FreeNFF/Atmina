14b3b97a3bd7d58645944901f1641a7aa1a64c8f
FreeNFF <nauisfricsons30@gmail.com>
2023-12-07 14:50:43 +0200

    1. stunda
    1. stunda

---
+++ atmina.py
@@ -0,0 +1,29 @@
+from tkinter import* #importē tkinter
+from PIL import ImageTk, Image
+import random
+from tkinter import messagebox
+
+#ielādēt pillow, lai varētu ielikt bildes: pip install pillow
+
+gameWindow=Tk()
+gameWindow.title("Atmiņas spēle")
+
+##########################################################################################################
+
+btn0=Button(width=200, height=300)#sāk no 0, jo saistās ar saraksta elementu indeksāciju
+btn1=Button(width=200, height=300)
+btn2=Button(width=200, height=300)
+btn3=Button(width=200, height=300)
+btn4=Button(width=200, height=300)
+btn5=Button(width=200, height=300)
+btn6=Button(width=200, height=300)
+btn7=Button(width=200, height=300)
+btn8=Button(width=200, height=300)
+btn9=Button(width=200, height=300)
+btn10=Button(width=200, height=300)
+btn11=Button(width=200, height=300)
+
+
+##########################################################################################################
+
+gameWindow.mainloop()
