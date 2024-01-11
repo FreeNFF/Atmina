1e2aca42f3702e9ba91e2577eb96a40029361d41
FreeNFF <nauisfricsons30@gmail.com>
2024-01-11 14:49:05 +0200

    30123123324. stunda
    12423542311324. stunda

--- atmina.py
+++ atmina.py
@@ -10,6 +10,7 @@ gameWindow.title("Atmiņas spēle")
 
 #######################################################################################################
 count=0
+winner=0
 correctAnswers=0
 answers=[]
 answer_dict={}#kas ir piespiests, salīdzinās ar attēliem no saraksta
@@ -34,10 +35,50 @@ ImageList=[myImg1, myImg1, myImg2, myImg2, myImg3, myImg3, myImg4, myImg4, myImg
 
 random.shuffle(ImageList) #attēli jāsajauc(attēli tiek sajaukti nejaušā secībā)
 
+######################################################################################################################
+
+def reset():
+    global count, correctAnswers, answers, answer_dict, winner
+    count=0
+    winner=0
+    correctAnswers=0
+    answers=[]
+    answer_dict={}
+    btn0.config(state=NORMAL) #pogas stāvoklis izslēgts
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn10.config(state=NORMAL)
+    btn11.config(state=NORMAL)
+
+    btn0["image"]="pyimage7" #pogas attēlu maiņa
+    btn1["image"]="pyimage7"
+    btn2["image"]="pyimage7"
+    btn3["image"]="pyimage7"
+    btn4["image"]="pyimage7"
+    btn5["image"]="pyimage7"
+    btn6["image"]="pyimage7"
+    btn7["image"]="pyimage7"
+    btn8["image"]="pyimage7"
+    btn9["image"]="pyimage7"
+    btn10["image"]="pyimage7"
+    btn11["image"]="pyimage7"
+
+    random.shuffle(ImageList)
+
+    return
+
+
 ###################################################################################################################
 
 def btnClick(btn,number):
-    global count, correctAnswers, answers, answer_dict,winner
+    global count, correctAnswers, answers, answer_dict, winner
     if btn["image"] == "pyimage7" and count<2:#pēc sistēmas nosauc šādi'
         btn["image"]=ImageList[number]
         count+=1#viena rūtiņa atklāta
@@ -51,8 +92,7 @@ def btnClick(btn,number):
             if correctAnswers==2:
                 messagebox.showinfo("Vienādi attēli", "Esi uzminējis")
                 correctAnswers=0
-            if correctAnswers==6:
-                messagebox.showinfo("Vienādi attēli","Tu uzvarēji! Vai vēlies spēlēt vēreiz?")
+                winner+=1
         else:
             messagebox.showinfo("Vienādi attēli","Neuzminēji")
             for key in answer_dict:
@@ -60,7 +100,29 @@ def btnClick(btn,number):
         count=0
         answers=[]
         answer_dict={}
+    if winner==6:
+        messagebox.showinfo("Vienādi attēli","Tu uzvarēji! Vai vēlies spēlēt vēlteiz?")
+        reset()
     return
+
+############################################################################################################
+
+def infoLogs():
+    jaunsLogs=Toplevel()
+    jaunsLogs.title('Info par programmu')
+    jaunsLogs.geometry("680x200")
+    jaunsLogs.configure(bg='#ECFFDC')
+    apraksts=Label(jaunsLogs,text='Spēles noteikumi', font='Helvica 20 bold ', bg='#ECFFDC')#fonts, izmērs, stils
+    apraksts.grid(row=0,column=0)
+    apraksts=Label(jaunsLogs,text='Sākot spēli, spēlētājam jāapgriež divas kartiņas vienlaicīgi.', font='Helvica 14', bg='#ECFFDC')#fonts, izmērs, stils
+    apraksts.grid(row=1,column=0)
+    apraksts=Label(jaunsLogs,text='Ja simboli ir identiski, spēlētājs var minēt nākamos simbolus.', font='Helvica 14', bg='#ECFFDC')#fonts, izmērs, stils
+    apraksts.grid(row=2,column=0)
+    apraksts=Label(jaunsLogs,text='Ja simboli nav identiski, tās jāapgriež atpakaļ ar seju uz leju un var turpināt minēt.', font='Helvica 14', bg='#ECFFDC')#fonts, izmērs, stils
+    apraksts.grid(row=3,column=0)
+    apraksts=Label(jaunsLogs,text='Spēlētājs uzvar,. kad visus simbolus ir dabūjis vienādus.', font='Helvica 14', bg='#ECFFDC')#fonts, izmērs, stils
+    apraksts.grid(row=4,column=0)
+    return 
 
 ##########################################################################################################
 
@@ -79,7 +141,18 @@ btn11=Button(width=200,height=200,image=bgImg, command=lambda:btnClick(btn11,11)
 
 ##########################################################################################################
 
+#Lielā izvēle
+galvenaIzvele=Menu(gameWindow)#izveido galveno izvēli
+gameWindow.config(menu=galvenaIzvele)#pievieno galvenajam logam
 
+opcijas=Menu(galvenaIzvele,tearoff=False)#mazā izvēle
+galvenaIzvele.add_cascade(label="Opcijas", menu=opcijas)#lejupkrītošais saraksts
+galvenaIzvele.add_command(label="Par programmu",command=infoLogs) # pievieno mazajai izvēlnei vēl vienu izvēlni
+
+
+#Komandas
+opcijas.add_command(label="Jauna spēle", command=reset)
+opcijas.add_command(label="Iziet", command=gameWindow.quit)
 
 ##########################################################################################################
 
